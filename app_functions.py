cd586b02c737e15853b17ba0ddaf81a1d6659ffd
Shani <plant.shani@gmail.com>
2025-03-26 12:52:26 +0200

    fix: update import statement for KerasRegressor in app_functions.py

--- app_functions.py
+++ app_functions.py
@@ -4,8 +4,8 @@ import numpy as np
 import requests
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
-#from scikeras.wrappers import KerasRegressor
+#from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+from scikeras.wrappers import KerasRegressor
 #from keras.models import Sequential, load_model
 import joblib
 from scipy.signal import savgol_filter
